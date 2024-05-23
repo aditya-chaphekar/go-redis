176b7ce070c5db0264eeba7daab71a908a644f10
Aditya <adityachaps@gmail.com>
2024-05-24 01:42:53 +0530

    step-3: respond to multiple inputs

--- app/server.go
+++ app/server.go
@@ -10,15 +10,24 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listner, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	c, err := l.Accept()
-	c.Write([]byte("+PONG\r\n"))
+	connection, err := listner.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
+		fmt.Println("Error Accepting Connection")
 		os.Exit(1)
 	}
+	buff := make([]byte, 1024)
+	length, err := connection.Read(buff)
+	returnBuff := []byte("+PONG\r\n")
+	connection.Write(returnBuff)
+	for i := 0; i <= length; i++ {
+		if string(buff[i]) == "\n" {
+			connection.Write(returnBuff)
+		}
+	}
+
 }
